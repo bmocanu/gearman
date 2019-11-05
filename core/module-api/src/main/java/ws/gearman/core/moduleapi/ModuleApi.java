adfb18fc69f479ce2a0da4e9de17fab9861fca3b
Bogdan Mocanu <bogdan.mocanu@iquestgroup.com>
2019-11-05 18:05:34 +0200

    Create additional structure for the module-api and an initial module, the simple Timestamp module

---
+++ core/module-api/src/main/java/ws/gearman/core/moduleapi/ModuleApi.java
@@ -0,0 +1,7 @@
+package ws.gearman.core.moduleapi;
+
+public class ModuleApi {
+
+    // this will surely go away
+
+}
---
+++ modules/timestamp/src/main/java/ws/gearman/module/timestamp/TimestampModule.java
@@ -0,0 +1,4 @@
+package ws.gearman.module.timestamp;
+
+public class TimestampModule {
+}
